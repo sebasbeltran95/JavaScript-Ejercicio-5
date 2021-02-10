9a6c0bd793e12c50264e02af986a74fb0763d47f
sebastian beltran zapata <sebastian199502@hotmail.com>
2021-02-09 20:49:55 -0500

    JavaScript Ejercicio 5 Cofla

---
+++ codigo.js
@@ -0,0 +1,37 @@
+const sumar = (num1,num2) =>{
+    return parseInt(num1) + parseInt(num2);
+}
+const restar = (num1,num2) =>{
+    return parseInt(num1) - parseInt(num2);
+}
+const dividir = (num1,num2) =>{
+    return parseInt(num1) / parseInt(num2);
+}
+const multiplicar = (num1,num2) =>{
+    return parseInt(num1) * parseInt(num2);
+}
+alert("¿Que operacion deseas realizar?");
+let operacion = prompt("1: sumar, 2: restar, 3: dividir, 4: multiplicar");
+if(operacion == 1){
+    let numero1 = prompt("Primer numero para sumar")
+    let numero2 = prompt("Segundo numero para sumar");
+    resultado = sumar(numero1,numero2);
+    alert(`Tu resultado es: ${resultado}`);
+} else if(operacion == 2){
+    let numero1 = prompt("Primer numero para restar")
+    let numero2 = prompt("Segundo numero para restar");
+    resultado = restar(numero1,numero2);
+    alert(`Tu resultado es: ${resultado}`);
+}else if(operacion == 3){
+    let numero1 = prompt("Primer numero para dividir")
+    let numero2 = prompt("Segundo numero para dividir");
+    resultado = dividir(numero1,numero2);
+    alert(`Tu resultado es: ${resultado}`);
+} else if(operacion == 4){
+    let numero1 = prompt("Primer numero para multiplicar")
+    let numero2 = prompt("Segundo numero para multiplicar");
+    resultado = multiplicar(numero1,numero2);
+    alert(`Tu resultado es: ${resultado}`);
+}else{
+    alert("Error al digitar el comando");
+}
